bc97f2a9d8b3c872dea51c1cadf5135baa14f46c
username <fruttys@gmail.com>
2022-10-29 23:09:25 +0300

    add post ques/answer

--- nodejs/src/routes/v1/auth.js
+++ nodejs/src/routes/v1/auth.js
@@ -78,4 +78,46 @@ router.post("/login", async (req, res) => {
   }
 });
 
+router.post("/questions", async (req, res) => {
+  try {
+    const question = req.body;
+    if (question.user_id && question.question) {
+      const con = await mysql.createConnection(dbconfig);
+
+      const [response] = await con.execute(
+        `INSERT INTO questions (user_id, question) values (${con.escape(
+          question.user_id
+        )}, ${con.escape(question.question)})`
+      );
+      res.send(response);
+      await con.end();
+    } else {
+      res.status(400).send("Bad syntax");
+    }
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+router.post("/answers", async (req, res) => {
+  try {
+    const answer = req.body;
+    if (answer.user_id && answer.answer) {
+      const con = await mysql.createConnection(dbconfig);
+
+      const [response] = await con.execute(
+        `INSERT INTO answers (user_id, answer) values (${con.escape(
+          answer.user_id
+        )}, ${con.escape(answer.answer)})`
+      );
+      res.send(response);
+      await con.end();
+    } else {
+      res.status(400).send("Bad syntax");
+    }
+  } catch (error) {
+    console.error(error);
+  }
+});
+
 module.exports = router;
